36095d841117242fd53034be1f2e1c4f517a0e7c
tanmaydhelia <tanmaydhelia03@gmail.com>
2025-11-28 09:26:52 +0530

    Added: Microservices Project

---
+++ src/main/java/com/demo/micro/DemoMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.demo.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroServicesApplication.class, args);
+	}
+
+}
